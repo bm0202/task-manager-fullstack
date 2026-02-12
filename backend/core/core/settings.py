35ab07fbefc06f19d4e4239b1088456fc158e68f
Bharath Siva <bharath.siva@collins.com>
2026-02-12 17:06:42 +0530

    API created for Register, Login, and Refresh

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
-    'rest_framework_simplejwt'
-    'users'
+    'rest_framework_simplejwt',
+
+    'users',
 ]
 
 MIDDLEWARE = [
